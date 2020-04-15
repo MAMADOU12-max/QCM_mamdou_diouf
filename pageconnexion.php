c66cb16ff65b07ce2ad341b5e4af2361d6962cb9
Mamadou Diouf <modoudiouf535@gmail.com>
2020-04-15 15:26:32 +0100

    teste des commande de github

--- pageconnexion.php
+++ pageconnexion.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <title>Document</title>
+    <title>Page connexion </title>
     <link rel="stylesheet" href="asset/CSS/pageconnexion.css">
 </head>
 <body>
